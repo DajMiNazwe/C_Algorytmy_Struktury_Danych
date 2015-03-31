58bdbf8c5fd83647deb305b08204046c79283de6
Strugacki <mhallman94@gmail.com>
2015-03-31 18:01:30 +0200

    PRZEBUDOWA KONCEPCJI

--- AL10_5.c
+++ AL10_5.c
@@ -7,12 +7,15 @@ typedef struct wezel *WskNaWezel;
 typedef struct lista *WskNaListe;
 
 
+WskNaListe ELEMENTS;
+
 /*DEKLARACJA STRUKTURY WEZLA*/
 typedef struct wezel{
-    WskNaWezel lewy; //lewy syn
-    WskNaWezel prawy;//prawy syn
+    WskNaWezel left; //lewy syn
+    WskNaWezel right;//prawy syn
     WskNaWezel parent;//rodzic
     char znak;//znak
+    int ile_razy;//ilosc wystapien znaku
 }TWEZEL;
 
 
@@ -20,13 +23,69 @@ typedef struct wezel{
 typedef struct lista{
     WskNaListe prev; //element poprzedni z listy
     WskNaListe next;//element nastepny z listy
-    int ile_razy; //ilosc wystapien znaku
     WskNaWezel korzen;//struktura wezla
 }TLISTA;
 
 
+WskNaWezel nowyWezel(char klucz){
+WskNaWezel nowy=(WskNaWezel)malloc(sizeof(TWEZEL));
+nowy->left=NULL;
+nowy->right=NULL;
+nowy->parent=NULL;
+nowy->ile_razy=0;
+nowy->znak=klucz;
+return nowy;
+}
+
+
+
+void listaInit(){
+ELEMENTS=(WskNaListe)malloc(sizeof(TLISTA));
+ELEMENTS->prev=NULL;
+ELEMENTS->next=NULL;
+ELEMENTS->korzen=NULL;
+return ELEMENTS;
+}
+
+
+void wczytajPlik(){
+char klucz;
+FILE *plik;
+plik=fopen("tekst.txt","r");
+if(plik==NULL){
+    printf("NIE UDALO SIE WCZYTAC PLIKU!!\n");
+}else{
+
+
+}
+
+
+}
+
+
+
+WskNaListe uzupelnijListe(){
+
+WskNaListe temp=(WskNaListe)malloc(sizeof(TLISTA));
+
+
+ELEMENTS->korzen=nowyWezel('\t');
+temp=ELEMENTS;
+temp->korzen=nowyWezel('\n');
+ELEMENTS->
+
+
+
+
+return ELEMENTS;
+}
 
 int main(){
+
+
+listaInit();
+
+
 
 
 return 0;
