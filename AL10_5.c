39cf3ca3908c361624726e1385481b2e4f41c3c8
Strugacki <mhallman94@gmail.com>
2015-03-31 20:53:02 +0200

    BUDOWA LISTY I WCZYTYWANIE Z PLIKU ZROBIONE

--- AL10_5.c
+++ AL10_5.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+
+
 /*Pomocnicza deklaracja wskaznikow(skraca zapis)*/
 typedef struct wezel *WskNaWezel;
 typedef struct lista *WskNaListe;
@@ -27,6 +29,7 @@ typedef struct lista{
 }TLISTA;
 
 
+/*DODAWANIE NOWEGO WEZLA*/
 WskNaWezel nowyWezel(char klucz){
 WskNaWezel nowy=(WskNaWezel)malloc(sizeof(TWEZEL));
 nowy->left=NULL;
@@ -38,7 +41,7 @@ return nowy;
 }
 
 
-
+/*INICJALIZACJA LISTY*/
 void listaInit(){
 ELEMENTS=(WskNaListe)malloc(sizeof(TLISTA));
 ELEMENTS->prev=NULL;
@@ -48,27 +51,58 @@ return ELEMENTS;
 }
 
 
+/*FUNKCJA "ODPORWADZAJACA" WSKAZNIK NA POCZATEK LISTY*/
+void znajdzPoczatek(){
+
+ELEMENTS;
+while(ELEMENTS->prev!=NULL){
+    //printf("%i\n\n",ELEMENTS->korzen->znak);
+    ELEMENTS=ELEMENTS->prev;
+}
+ELEMENTS=ELEMENTS->next;
+return ELEMENTS;
+}
+
+/*WCZYTYWANIE PLIKU TZN ZNAKOW I ZLICZANIE ICH WYSTAPIEN*/
 void wczytajPlik(){
-char klucz;
-FILE *plik;
-plik=fopen("tekst.txt","r");
-if(plik==NULL){
-    printf("NIE UDALO SIE WCZYTAC PLIKU!!\n");
-}else{
+    char c;
+    WskNaListe poczatek;
+    FILE * plik;
+
+     plik=fopen("tekst.txt","r");
+      if(plik==NULL){
+        fclose(plik);
+        printf("->Brak pliku z tekstem\n");
+      }
+      else{
+        printf("->Wykryto plik z tekstem\n");
+        c=getc(plik);
+        while(c!=EOF){//pobieranie znaku
+        znajdzPoczatek();
+        while(ELEMENTS->next!=NULL){//przeszukiwanie listy
+           if(ELEMENTS->korzen->znak==c){
+               ELEMENTS->korzen->ile_razy++;//zliczanie wystapien
+               printf("znak:%c | ilosc wystapien: %i\n",ELEMENTS->korzen->znak,ELEMENTS->korzen->ile_razy);
+            }
+            ELEMENTS=ELEMENTS->next;
+        }
+           c=getc(plik);//pobieranie nastepnego znaku
+        }
+        znajdzPoczatek();
+        fclose(plik);
+    }
 
 
 }
 
-
-}
-
-
+/*FUNKCJA POMOCNICZA WSTAWIAJACA WEZEL DO LISTY*/
 WskNaListe wstawDoListy(char klucz){
 WskNaListe tmp=(WskNaListe)malloc(sizeof(TLISTA));
 
+/*sprawdzanie czy lista jest pusta czy nie*/
 if(ELEMENTS==NULL){
 ELEMENTS->korzen=nowyWezel(klucz);
-}else{  //jesli nie to przenosimy uklad do zmiennej tymczasowej
+}else{
 tmp->korzen=nowyWezel(klucz);
 tmp->prev=ELEMENTS;
 ELEMENTS->next=tmp;
@@ -78,7 +112,7 @@ return ELEMENTS;
 }
 
 
-
+/*FUNKCJA UZUPELNIAJACA LISTE WEZLAMI*/
 WskNaListe uzupelnijListe(){
 
 WskNaListe temp=(WskNaListe)malloc(sizeof(TLISTA));
@@ -86,18 +120,25 @@ WskNaListe temp=(WskNaListe)malloc(sizeof(TLISTA));
 int zakres;
 char znak;
 
+/*biale znaki*/
 wstawDoListy('\t');
 wstawDoListy('\n');
 wstawDoListy(' ');
+/*cyfry*/
+for(zakres='0';zakres<='9';zakres++){
+    znak=zakres;
+    wstawDoListy(znak);
+}
+/*wielkie litery*/
 for(zakres='A';zakres<='Z';zakres++){
     znak=zakres;
     wstawDoListy(znak);
 }
+/*male litery*/
 for(zakres='a';zakres<='z';zakres++){
     znak=zakres;
     wstawDoListy(znak);
 }
-printf("ostatni: %c przedostatni: %c biezacy: %c",ELEMENTS->prev->prev->korzen->znak,ELEMENTS->prev->korzen->znak,ELEMENTS->korzen->znak);
 return ELEMENTS;
 }
 
@@ -106,6 +147,7 @@ int main(){
 
 listaInit();
 uzupelnijListe();
+wczytajPlik();
 
 
 
